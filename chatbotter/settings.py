ca9d2bd01c0d4d8efae1601a7729da0a0c5f9da4
Santigio <santigiesankoh233@gmail.com>
2022-12-14 22:35:10 +0200

    add

--- chatbotter/settings.py
+++ chatbotter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&5%8lvz#d7hx32+i3wny74-c$g%vx^mo*ki)zkwbd8h8awoqta
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app'] # Allow *.vercel.app
 
 
 # Application definition
