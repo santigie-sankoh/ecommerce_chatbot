2caeb98f3297a8371238ff8e8af54cccab01af01
Santigio <santigiesankoh233@gmail.com>
2022-12-15 10:26:17 +0200

    chatbot model

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+# Import necessary modules and libraries
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+# Import the chatbot model and functions
+from chatbot_model import predict, train
+
 
 # Create your views here.
 
@@ -9,3 +17,18 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
+
+@csrf_exempt
+def chatbot_view(request):
+    # Parse the JSON data from the request
+    data = json.loads(request.body)
+    
+    # Get the user input from the request data
+    user_input = data['user_input']
+    
+    # Use the chatbot model to generate a response
+    response = predict(user_input)
+    
+    # Return the response as a JSON object
+    return HttpResponse(json.dumps({'response': response}), content_type='application/json')
